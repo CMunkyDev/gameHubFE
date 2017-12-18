5896f65d97ee1d191414549f84bd10932ab32e69
CMunkyDev <matt@jcmueller.com>
2017-12-18 12:10:05 -0800

    add starting component folders and files

---
+++ gamehub/src/components/UserPage.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react'
+
+class UserPage extends Component {
+
+    render () {
+        return (
+            <div className = "container-fluid">
+                <div className = "row">
+                    //HEADER
+                </div>
+                <div className = "row">
+                    //USER INFO
+                </div>
+                <div className = "row">
+                    //SERVICE BUTTON BAR
+                </div>
+                <div className = "row">
+                    //GAME ROW CONTAINER
+                </div>
+                <div className = "row">
+                    //FOOTER
+                </div>
+            </div>
+        )
+    }
+}
+
+export default UserPage
