d47e6ce9e8bd726a2f219d080392c78a0c6d9894
CMunkyDev <matt@jcmueller.com>
2017-12-19 11:31:35 -0800

    create maybe terrible and unnecessary button and dropdown components

--- gamehub/src/components/generic/Button.js
+++ gamehub/src/components/generic/Button.js
@@ -1,3 +1,14 @@
 import React from 'react'
 
+const Button = ({text, attributes, callback}) => {
+    if (callback) {
+        attributes.onClick = callback
+    }
+    return (
+        <button {...attributes}>
+            {text}
+        </button>
+    )
+}
+
 export default Button
--- gamehub/src/components/generic/Dropdown.js
+++ gamehub/src/components/generic/Dropdown.js
@@ -1,3 +1,17 @@
 import React from 'react'
 
+//optionsObject is an object where the options are the keys and the values are the... values... {option: value, option: value}
+const Dropdown = ({ optionsObject, attributes, initialValue, defaultSelected}) => {
+    let options = []
+    for (let key in optionsObject) {
+        options = [...options, <option value={optionsObject[key]}>{key}</option>]
+    }
+    options = [<option disabled value={''} selected>{initialValue || 'Select'}</option>, ...options]
+    return (
+        <select {...attributes} defaultValue = {defaultSelected || ''}>
+            {options}
+        </select>
+    )
+}
+
 export default Dropdown
