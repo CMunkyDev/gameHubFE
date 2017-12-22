d0777db028cd17761f692272a5b9048b0dc618d2
Justin Hays <jhays124@yahoo.com>
2017-12-21 16:26:30 -0800

    thumbnail icons and select stats now render

--- gamehub/src/components/UserPage.js
+++ gamehub/src/components/UserPage.js
@@ -1,6 +1,18 @@
 import React, { Component } from 'react'
+import ServiceContainer from './UserPage/ServiceContainer'
+let steamCall = require('../fakeNews').response
+
 
 class UserPage extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      services: [{name: 'steam'}],
+      favoriteGames:{'steam': [226700, 224760, 252950]},
+      currentService: 0,
+      userGames: {'steam': [...steamCall.games]}
+    }
+  }
 
     render () {
         return (
@@ -15,7 +27,7 @@ class UserPage extends Component {
                     //SERVICE BUTTON BAR
                 </div>
                 <div className = "row">
-                    //GAME ROW CONTAINER
+                  < ServiceContainer favoriteGames={this.state.favoriteGames[this.state.services[this.state.currentService].name]} userGames={this.state.userGames[this.state.services[this.state.currentService].name]}/>
                 </div>
                 <div className = "row">
                     //FOOTER
--- gamehub/src/components/UserPage/ServiceContainer/GameRow.js
+++ gamehub/src/components/UserPage/ServiceContainer/GameRow.js
@@ -1,3 +1,14 @@
 import React from 'react'
 
+const GameRow = ({game}) => {
+  return (
+    <div className='container-fluid'>
+      <img src={`http://media.steampowered.com/steamcommunity/public/images/apps/${game.appid}/${game.img_icon_url}.jpg`}></img>
+      <span>{game.name}</span>
+      <span>Total time {(game.playtime_forever/60).toFixed(1)} hours </span>
+      <span>Last fortnight {(game.playtime_2weeks) ? (game.playtime_2weeks/60).toFixed(1) : 'Not played' }</span>
+    </div>
+  )
+}
+
 export default GameRow
