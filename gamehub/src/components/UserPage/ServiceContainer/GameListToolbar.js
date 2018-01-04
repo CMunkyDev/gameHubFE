df04a1086aa0cb6853a340b270cd7a48889c29ce
CMunkyDev <matt@jcmueller.com>
2018-01-03 23:49:34 -0800

    get basic toolbar layout started

--- gamehub/src/components/UserPage/ServiceContainer/GameListToolbar.js
+++ gamehub/src/components/UserPage/ServiceContainer/GameListToolbar.js
@@ -1,25 +1,36 @@
 import React, { Component } from 'react'
 import { Toolbar, ToolbarGroup, ToolbarSeparator, ToolbarTitle } from 'material-ui/Toolbar';
+import TextField from 'material-ui/TextField'
+import Toggle from 'material-ui/Toggle'
+import FlatButton from 'material-ui/FlatButton'
+
 
 class GameListToolbar extends Component {
     constructor (props) {
         super(props)
+        this.arrowIconDown = 'DESC'
+        this.arrowIconUp = 'ASC'
 
+        this.state = {
+            nameSortButtonIcon: this.arrowIconUp,
+            timeSortButtonIcon: this.arrowIconUp
+        }
     }
 
     render () {
         return (
             <Toolbar >
-                <ToolbarGroup>
-                    SORT
+                <ToolbarGroup style={{ display: 'flex', width: '28%'}}>
+                    <FlatButton label="Game Name"/>
+                    <FlatButton label={this.state.nameSortButtonIcon}/>
+                    <FlatButton label="Time Played" />
+                    <FlatButton label={this.state.timeSortButtonIcon} />
                 </ToolbarGroup>
-                <ToolbarSeparator />
-                <ToolbarGroup>
-                    FILTER
+                <ToolbarGroup style={{ display: 'flex', width: '18%'}}>
+                    <Toggle label='Show Unplayed Games'/>
                 </ToolbarGroup>
-                <ToolbarSeparator />
-                <ToolbarGroup>
-                    SEARCH
+                <ToolbarGroup style={{ display: 'flex', width: '28%' }}>
+                    <TextField hintStyle={{color:'pink'}} hintText='Search...'/>
                 </ToolbarGroup>
             </Toolbar>
         )
